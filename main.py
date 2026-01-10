fd5f75da197892defded907846c590ab8452d4cf
Giovani Gerci <giiovanigerci@gmail.com>
2026-01-10 15:42:40 -0300

    Refatorar a saida do endpoint /network para retorno estruturado em JSON

--- main.py
+++ main.py
@@ -6,6 +6,61 @@ import os
 app = FastAPI(tittle="Monitor de Rede")
 PING_TARGET = os.getenv("PING_TARGET", "8.8.8.8")
 
+def run_command(command: str) -> str:
+	result = subprocess.run(
+		command,
+		shell=True,
+		capture_output=True,
+		text=True
+	)
+	return result.stdout.strip()
+
+
+def get_ip_info():
+	output = run_command("ip -4 addr show eth0")
+
+	for line in output.splitlines():
+		if "inet" in line:
+			parts = line.split()
+			ip, mask = parts[1].split("/")
+			return {
+				"ip": ip,
+				"mask": mask
+			}
+		
+	return None
+
+
+def get_gateway():
+	output = run_command("ip route")
+
+	for line in output.splitlines():
+		if line.startswith("default"):
+			return line.split()[2]
+		
+	return None
+
+
+def ping_target():
+	output = run_command(f"ping -c 1 {PING_TARGET}")
+
+	if "time=" in output:
+		for line in output.splitlines():
+			if "time=" in line:
+				time_ms = line.split("time=")[1].split(" ")[0]
+				return {
+					"target": PING_TARGET,
+					"success": True,
+					"time_ms": float(time_ms)
+				}
+
+	return{
+		"target": PING_TARGET,
+		"success": False,
+		"time_ms": None
+	}		
+
+
 @app.get("/")
 def root():
 	return {"status": "API de Monitoramento de Rede rodando"}
@@ -16,15 +71,9 @@ def health():
 
 @app.get("/network")
 def network():
-	ip_result = subprocess.getoutput("ip -4 addr show | grep inet | grep -v 127.0.0.1")
-
-	gateway = subprocess.getoutput("ip route | grep default")
-
-	ping = subprocess.getoutput(f"ping -c 1 {PING_TARGET}")
-
 	return {
 		"timestamp": datetime.datetime.now().isoformat(),
-		"ip_info": ip_result,
-		"gateway": gateway,
-		"ping_test": ping
-		}
+		"ip_info": get_ip_info(),
+		"gateway": get_gateway(),
+		"ping": ping_target()
+	}
